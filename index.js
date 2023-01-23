fe1865f716565afbfb2bd7071eca7478a0b392d2
eerenyilmaz <eeyilmaz34@gmail.com>
2023-01-23 14:03:50 +0300

    Baplantı sayısını kontrol için counter eklendi

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let counter = 0;
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
     socket.broadcast.emit('hi');
+    counter++;
+    console.log(counter)
   });
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
-      console.log(msg)
     });
   });
 
